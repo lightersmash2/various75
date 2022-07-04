8ad49c951ba62bc0c44b50b899405c3405673f7b
lightersmash2 <86865909+lightersmash2@users.noreply.github.com>
2022-07-04 06:04:21 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const app = express()
-app.get("/test", (request, response) => {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("nerd test");
-  response.end();
+const Jimp = require("jimp");
+const imgcheck = require("is-an-image-url");
+const urlvalid = require("url-valid");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/main.html");
 });
+    
+const listener = app.listen(process.env.PORT, function() {});
