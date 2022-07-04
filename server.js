af850a21062366e8a814bdcc086543995139eb3a
lightersmash2 <86865909+lightersmash2@users.noreply.github.com>
2022-07-04 05:56:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express()
-app.get("/", (request, response) => {
+app.get("/test", (request, response) => {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("nerd test");
   response.end();
