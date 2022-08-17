eb76c60af02c5e23fd7295578ffc90393f588a4a
lightersmash2 <86865909+lightersmash2@users.noreply.github.com>
2022-08-17 02:38:17 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/main.html");
+app.get("*", (request, response) => {
+  response.send(request.originalUrl);
 });
-    
+app.post("*", (request, response) => {
+  response.send(request.originalUrl);
+});
+app.patch("*", (request, response) => {
+  response.send(request.originalUrl);
+}); 
+app.delete("*", (request, response) => {
+  response.send(request.originalUrl);
+});
 const listener = app.listen(process.env.PORT, function() {}); // keeping there for stability
