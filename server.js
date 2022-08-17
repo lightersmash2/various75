88a14765b0fc5939616b3c20a0b84632c9c6e777
lightersmash2 <86865909+lightersmash2@users.noreply.github.com>
2022-08-17 02:52:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
+var http = require('http');
 
 const app = express();
 
-app.get("/:page", (request, response) => {
-  var id = request.params.page;
-  // do something with id
-    // send a response to user based on id
-    var obj = { id : id, Content : "content " +id };
+app.get("*", (request, response) => {
+  //The url we want is: 'www.random.com/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var options = {
+  host: 'www.roblox.com',
+  path: '/'
+};
+var str = '';
+callback = function(response) {
 
-    response.writeHead(200, {"Content-Type": "application/json"});
-    response.write(JSON.stringify(obj));
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+http.request(options, callback).end();
+response.send(str)
 });
 app.post("*", (request, response) => {
   response.send(request.originalUrl);
